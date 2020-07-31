c91f41e0fb6fa1a1c1d96c3e9c5be63b5188fa7c
Pedro Durek <pedro_hmd@hotmail.com>
2020-07-30 19:52:18 -0500

    Add support to alias

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,21 @@ module.exports = {
     es2020: true,
     node: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb'],
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src', '@types'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  plugins: ['react', 'react-hooks', 'prettier', '@typescript-eslint'],
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'eslint:recommended',
+    'plugin:import/typescript',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
@@ -13,15 +27,26 @@ module.exports = {
     ecmaVersion: 11,
     sourceType: 'module',
   },
-  plugins: ['react', '@typescript-eslint'],
+  ignorePatterns: ['webpack*.js'],
   rules: {
     'react/jsx-filename-extension': 'off',
     'no-unused-vars': 'off',
+    'react/prop-types': 'off',
     '@typescript-eslint/no-unused-vars': [
       'error',
       {
         argsIgnorePattern: '^_',
         varsIgnorePattern: '^_',
+      },
+    ],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
       },
     ],
   },
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   presets: ['@babel/env', '@babel/typescript', '@babel/react'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          res: './src/res',
+          contexts: './src/contexts',
+          views: './src/views',
+        },
+      },
+    ],
+  ],
 };
