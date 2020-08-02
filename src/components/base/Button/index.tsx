8209fea69b42198672eef8c20037430d8ec36086
Pedro Durek <pedro_hmd@hotmail.com>
2020-08-01 23:12:03 -0500

    Add button component

---
+++ src/components/base/Button/index.tsx
@@ -0,0 +1,29 @@
+import styled, { css } from 'styled-components';
+import hexToRgba from 'hex-to-rgba';
+
+type Props = {
+  variant: 'primary' | 'secondary';
+};
+
+const Button = styled.button(
+  ({ theme: { dimens, colors } }) => css`
+    font-size: 16px;
+    background-color: ${colors.primary};
+    color: ${colors.white};
+    padding: ${dimens.spacing.small}px ${dimens.spacing.small2}px;
+    border-radius: ${dimens.radius.small}px;
+    border: 0;
+    cursor: pointer;
+    outline: none;
+    transition: all 0.15s ease-in-out;
+    :focus,
+    :hover {
+      background-color: ${colors.primaryDarker};
+    }
+    :focus {
+      box-shadow: 0 0 0 0.2rem ${hexToRgba(colors.primary, 0.5)};
+    }
+  `,
+);
+
+export default Button;
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,11 @@
 import React, { FC } from 'react';
 import ReactDOM from 'react-dom';
 import ThemeProvider from 'contexts/ThemeProvider';
-import styled from 'styled-components';
-
-const Test = styled.span`
-  color: ${({ theme }) => theme.colors.primary};
-`;
+import Routes from './Routes';
 
 const App: FC = () => (
   <ThemeProvider>
-    <div>
-      <Test>Hello World!</Test>
-    </div>
+    <Routes />
   </ThemeProvider>
 );
 
