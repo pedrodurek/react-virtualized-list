d16c9a6073cf401f9d1ba90d4f4ff6fee1851cd9
Pedro Durek <pedro_hmd@hotmail.com>
2020-08-01 23:15:54 -0500

    Add extra colors

--- src/res/themes/lightTheme.ts
+++ src/res/themes/lightTheme.ts
@@ -1,7 +1,10 @@
 const pallete = {
+  primaryLighter: '#ac2033',
   primary: '#cc263c',
+  primaryDarker: '#ac2033',
   black: '#343a40',
   white: '#fafafa',
+  grey: '#dee2e6',
 };
 
 export default pallete;
