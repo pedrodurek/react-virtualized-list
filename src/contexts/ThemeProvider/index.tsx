12d50e7d33c9fcb837eafb289333bcc11649d95e
Pedro Durek <pedro_hmd@hotmail.com>
2020-08-01 23:12:43 -0500

    Add global styles

--- src/contexts/ThemeProvider/index.tsx
+++ src/contexts/ThemeProvider/index.tsx
@@ -1,8 +1,17 @@
 import React, { FC } from 'react';
-import { ThemeProvider as StyledComponentsThemeProvider } from 'styled-components';
+import {
+  ThemeProvider as StyledComponentsThemeProvider,
+  createGlobalStyle,
+} from 'styled-components';
 import pallete from 'res/themes/lightTheme';
 import dimens from 'res/dimens';
 import { AppTheme } from './types';
+
+const GlobalStyle = createGlobalStyle`
+  html, body {
+    margin: 0;
+  }
+`;
 
 const ThemeProvider: FC = ({ children }) => {
   const theme: AppTheme = {
@@ -12,6 +21,7 @@ const ThemeProvider: FC = ({ children }) => {
 
   return (
     <StyledComponentsThemeProvider theme={theme}>
+      <GlobalStyle />
       {children}
     </StyledComponentsThemeProvider>
   );
