6db1287037b1f2f511985034549e1a5215ba0376
Pedro Durek <pedro_hmd@hotmail.com>
2020-08-01 23:15:35 -0500

    Add extra dimens

--- src/res/dimens.ts
+++ src/res/dimens.ts
@@ -1,13 +1,20 @@
 const spacing = {
   tiny: 2,
   small: 4,
-  regular: 8,
-  medium: 16,
-  large: 32,
+  small2: 8,
+  medium: 12,
+  medium2: 16,
+  large: 24,
+  large2: 32,
+};
+
+const radius = {
+  small: 4,
 };
 
 const dimens = {
   spacing,
+  radius,
 };
 
 export default dimens;
