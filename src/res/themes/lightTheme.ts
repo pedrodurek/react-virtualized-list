3a7ec1a3b137e51426d26e99a3eb387ad748b9a1
Pedro Durek <pedro_hmd@hotmail.com>
2020-08-03 21:41:43 -0500

    Add background color

--- src/res/themes/lightTheme.ts
+++ src/res/themes/lightTheme.ts
@@ -1,10 +1,12 @@
 const pallete = {
   primaryLighter: '#ac2033',
+  background: '#fafafa',
   primary: '#cc263c',
   primaryDarker: '#ac2033',
   black: '#343a40',
   white: '#fafafa',
   grey: '#dee2e6',
+  lightGrey: '#ebecf0',
 };
 
 export default pallete;
